588a97f4a840114622945cb69f099bebfaaaa818
D1maD1m0nd <d1m0hkrasav4ik@gmail.com>
2023-02-07 16:33:09 +0300

    add impl angular-visa-cost.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { HttpClientModule } from '@angular/common/http'
+import { AgGridModule } from 'ag-grid-angular';
 import { AppComponent } from './app.component';
 import { AngularVisaCostComponent } from './components/angular-visa-cost/angular-visa-cost.component';
-import { HttpClientServiceComponent } from './services/http-client-service/http-client-service.component';
-import { ErrorServiceComponent } from './services/error-service/error-service.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AngularVisaCostComponent,
-    HttpClientServiceComponent,
-    ErrorServiceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AgGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/angular-visa-cost/angular-visa-cost.component.ts
+++ src/app/components/angular-visa-cost/angular-visa-cost.component.ts
@@ -1,5 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  CellEditingStartedEvent, CellEditingStoppedEvent,
+  ColDef,
+  GridApi,
+  GridReadyEvent,
+  RowEditingStartedEvent,
+  RowEditingStoppedEvent
+} from "ag-grid-community";
+import {HttpClient} from "@angular/common/http";
+import "ag-grid-enterprise"
+export interface IOlympicData {
+  athlete: string;
+  age: number;
+  country: string;
+  year: number;
+  date: string;
+  sport: string;
+  gold: number;
+  silver: number;
+  bronze: number;
+  total: number;
+}
 @Component({
   selector: 'app-angular-visa-cost',
   templateUrl: './angular-visa-cost.component.html',
@@ -7,9 +28,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AngularVisaCostComponent implements OnInit {
 
-  constructor() { }
+  private gridApi!: GridApi;
+  public columnDefs: ColDef[] = [
 
+    { field: 'country', rowGroup: true, hide: true, enableRowGroup: true, filter: 'agSetColumnFilter'},
+    { field: 'year', rowGroup: true, hide: true, enableRowGroup: true, filter: 'agSetColumnFilter',  },
+    { field: 'gold', aggFunc: 'sum', headerName: 'Золото',filter: 'agSetColumnFilter', },
+    { field: 'silver', aggFunc: 'max', headerName: 'Серебро', filter: 'agSetColumnFilter',},
+    { field: 'bronze', aggFunc: 'avg', headerName: 'Бронза', filter: 'agSetColumnFilter',},
+  ];
+  public defaultColDef: ColDef = {
+    flex: 1,
+    minWidth: 100,
+    sortable: true,
+    resizable: true,
+    editable: true,
+  };
+  public autoGroupColumnDef: ColDef = {
+    minWidth: 200,
+  };
+  public rowGroupPanelShow: 'always' | 'onlyWhenGrouping' | 'never' = 'always';
+  public rowData!: IOlympicData[];
+
+  constructor(private http: HttpClient) {}
+
+  onGridReady(params: GridReadyEvent<IOlympicData>) {
+    this.http
+      .get<IOlympicData[]>(
+        'https://www.ag-grid.com/example-assets/olympic-winners.json'
+      )
+      .subscribe((data) => {
+        this.rowData = data
+        console.log(data)
+      });
+  }
+  onRowEditingStarted(event: RowEditingStartedEvent) {
+    console.log('never called - not doing row editing');
+  }
+
+  onRowEditingStopped(event: RowEditingStoppedEvent) {
+    console.log('never called - not doing row editing');
+  }
+
+  onCellEditingStarted(event: CellEditingStartedEvent) {
+    console.log('cellEditingStarted');
+  }
+
+  onCellEditingStopped(event: CellEditingStoppedEvent) {
+    console.log('cellEditingStopped');
+  }
   ngOnInit(): void {
   }
+
 
 }
