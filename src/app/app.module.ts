17d1e5a1efa77a2ec48ba839d547a5d3dbccba17
D1maD1m0nd <d1m0hkrasav4ik@gmail.com>
2023-02-07 16:22:13 +0300

    add error service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AngularVisaCostComponent } from './components/angular-visa-cost/angular-visa-cost.component';
+import { HttpClientServiceComponent } from './services/http-client-service/http-client-service.component';
+import { ErrorServiceComponent } from './services/error-service/error-service.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AngularVisaCostComponent
+    AngularVisaCostComponent,
+    HttpClientServiceComponent,
+    ErrorServiceComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/services/error-service/error-service.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-error-service',
+  templateUrl: './error-service.component.html',
+  styleUrls: ['./error-service.component.scss']
+})
+export class ErrorServiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
