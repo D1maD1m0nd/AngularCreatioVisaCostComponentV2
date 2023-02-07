c8384f33db36fcb7911a1ac2b497a13dd5638a77
D1maD1m0nd <d1m0hkrasav4ik@gmail.com>
2023-02-07 16:22:29 +0300

    add http client service

---
+++ src/app/services/http-client-service/http-client-service.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-http-client-service',
+  templateUrl: './http-client-service.component.html',
+  styleUrls: ['./http-client-service.component.scss']
+})
+export class HttpClientServiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
