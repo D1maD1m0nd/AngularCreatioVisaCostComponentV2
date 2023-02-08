68d0b3b5aff88d25efc60f25fbdc1a53016dd767
D1maD1m0nd <d1m0hkrasav4ik@gmail.com>
2023-02-08 11:08:31 +0300

    edit config column

--- src/app/utils/mapper/ColumnMapper.ts
+++ src/app/utils/mapper/ColumnMapper.ts
@@ -1,24 +1,36 @@
 import {ICostColumn} from "../../data/model/response/CostColumn";
 import {ColDef} from "ag-grid-community";
 
-const invisibleColumn : string[] = [
-  "DetailBudgetId",
-  "YearName",
-  "BrandName"
+const invisibleColumn: string[] = [
+    "DetailBudgetId",
+    "YearName",
+    "BrandName",
+    "FirstQuarterPlanSumBranch",
+    "SecondQuarterPlanSumBranch",
+    "ThirdQuarterPlanSumBranch",
+    "FourthQuarterPlanSumBranch"
 ]
-export function ToColumnDefArr(CostItemColumn: ICostColumn[], instance : any) : ColDef[] {
-  return CostItemColumn.map((i) =>{
-      const isString = typeof instance[i.ItemCostKey] === 'string';
-      const isInvisible = invisibleColumn.includes(i.ItemCostKey);
-      return <ColDef> {
-        field: i.ItemCostKey,
-        headerName: i.Name,
-        editable: !isString,
-        enableRowGroup: isString,
-        filter: isString ? 'agSetColumnFilter' : null,
-        aggFunc: !isString ? 'sum' : null,
-        hide: isInvisible
-      }
-    }
-  );
+
+const editableColumn: string[] = [
+    "FirstQuarterNewSum",
+    "SecondQuarterNewSum",
+    "ThirdQuarterNewSum",
+    "FourthQuarterNewSum"
+]
+export function ToColumnDefArr(CostItemColumn: ICostColumn[], instance: any): ColDef[] {
+    return CostItemColumn.map((i) => {
+            const isString = typeof instance[i.ItemCostKey] === 'string';
+            const isInvisible = invisibleColumn.includes(i.ItemCostKey);
+            const isEditable = editableColumn.includes(i.ItemCostKey);
+            return <ColDef>{
+                field: i.ItemCostKey,
+                headerName: i.Name,
+                editable: isEditable,
+                enableRowGroup: isString,
+                filter: isString ? 'agSetColumnFilter' : null,
+                aggFunc: !isString ? 'sum' : null,
+                hide: isInvisible
+            }
+        }
+    );
 }
