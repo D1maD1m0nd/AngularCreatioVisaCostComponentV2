6ec86553904635cf84c6e9d4dac50213996942e4
D1maD1m0nd <d1m0hkrasav4ik@gmail.com>
2023-02-08 09:56:13 +0300

    update model

--- src/app/data/model/response/ItemCost.ts
+++ src/app/data/model/response/ItemCost.ts
@@ -5,9 +5,17 @@ export interface ICostItem {
     OwnerName: string
     BrandName: string
     FirstQuarterPlanSum: number
+    FirstQuarterNewSum: number
+    FirstQuarterPlanSumBranch: number
     SecondQuarterPlanSum: number
+    SecondQuarterNewSum: number
+    SecondQuarterPlanSumBranch: number
     ThirdQuarterPlanSum: number
+    ThirdQuarterNewSum: number
+    ThirdQuarterPlanSumBranch: number
     FourthQuarterPlanSum: number
+    FourthQuarterNewSum: number
+    FourthQuarterPlanSumBranch: number
     TotalSumPlan: number
     TotalSumPlanYearBranch: number
     DetailBudgetId: string
