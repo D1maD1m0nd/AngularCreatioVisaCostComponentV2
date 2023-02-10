3ac9aafc4b1121c0be293b22d6fa6a73575e3210
D1maD1m0nd <d1m0hkrasav4ik@gmail.com>
2023-02-10 09:50:58 +0300

    add checkbox component

---
+++ src/app/components/checkbox-render-component/checkbox-render-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-checkbox-render-component',
+  templateUrl: './checkbox-render-component.component.html',
+  styleUrls: ['./checkbox-render-component.component.scss']
+})
+export class CheckboxRenderComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
